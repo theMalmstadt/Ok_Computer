3d1a27e73030fdab1a4754526dfe05d8bb8c970a
Ashlyn <asantiago17@wou.edu>
2020-02-14 21:41:02 -0800

    Fixed issues related to connection strings

--- OBM/Models/IdentityModels.cs
+++ OBM/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace OBM.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EventContext", throwIfV1Schema: false)
         {
         }
 
