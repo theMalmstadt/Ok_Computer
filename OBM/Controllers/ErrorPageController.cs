932a5ec4c9d933f19bb6466f3c10509a3fe9acfe
Expanseman <ethan.blackmail@gmail.com>
2020-02-16 13:04:50 -0800

    created custom error pages

---
+++ OBM/Controllers/ErrorPageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OBM.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        // GET: ErrorPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Error(int statusCode, Exception exception)
+        {
+            Response.StatusCode = statusCode;
+            ViewBag.StatusCode = statusCode + " Error";
+            ViewBag.Exception = exception;
+            return View();
+        }
+    }
+}
