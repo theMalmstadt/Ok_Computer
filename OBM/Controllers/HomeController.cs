e6f2ec732fee8c5c429c6567fc691bb8edb5bbbd
Expanseman <ethan.blackmail@gmail.com>
2020-02-22 18:04:24 -0800

    removed unnessary files; updated home and about page

--- OBM/Controllers/HomeController.cs
+++ OBM/Controllers/HomeController.cs
@@ -34,15 +34,6 @@ namespace OBM.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
