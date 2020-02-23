d2c3eba80068b5929f6b579fdfe8d18e73a4a27c
chemotroph <zakkeipp@gmail.com>
2020-02-23 13:47:37 -0800

    Renamed tournamentModel.cs to tournament.cs to match the formatting standards. Setup for continuous deplayment on DEV

--- OBM/Models/Tournament.cs
+++ OBM/Models/Tournament.cs
@@ -1,5 +1,6 @@
-namespace OBM.Models
+﻿namespace OBM.Models
 {
+
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
@@ -9,26 +10,25 @@ namespace OBM.Models
     [Table("Tournament")]
     public partial class Tournament
     {
+
         public int TournamentID { get; set; }
 
         [Required]
         [StringLength(128)]
         public string TournamentName { get; set; }
 
+        [Required]
         public int EventID { get; set; }
+
+        public String Game { get; set; }
+        public String ApiId  { get; set; }
+
+        public String UrlString { get; set; }
 
         [StringLength(256)]
         public string Description { get; set; }
+        public Boolean IsTeams { get; set; }
 
-        [StringLength(256)]
-        public string Game { get; set; }
 
-        public int? ApiId { get; set; }
-
-        [Required]
-        [StringLength(256)]
-        public string UrlString { get; set; }
-
-        public bool IsTeams { get; set; }
     }
 }
