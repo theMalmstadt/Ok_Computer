a70b583e0d3179eb69eb26f490d4757f499d5eb1
Expanseman <ethan.blackmail@gmail.com>
2020-03-07 15:57:57 -0800

    can now get match data from json endpoint

--- OBM/Scripts/competitorMatches.js
+++ OBM/Scripts/competitorMatches.js
@@ -1,14 +1,41 @@
 ﻿var interval = 1000 * 5;
 
-var ajaxCompetitors = function () {
+var ajaxMatches = function () {
+    var id = $('#EventID').val();
     $.ajax({
         type: 'GET',
         dataType: 'json',
-        url: '/Events/MatchList',
-        success: getTournamentList,
+        url: '/Events/Matches/' + id,
+        success: dispMatches,
+        complete: setTimeout(ajaxMatches, interval),
         error: errorOnAjax
     });
 }
-function errorOnAjax() {
-    console.log('Error on AJAX return');
+
+function dispMatches(jsonString) {
+    //console.log(jsonString["data"][0]);
+    var matchArray = jsonString["data"];
+    var id = $('#compID').val();
+    $("#matches").empty();
+    var arrLength = matchArray.length;
+    for (var i = 0; i < arrLength; i++) {
+        //console.log(matchArray[i]);
+        if ((id == matchArray[i].Competitor1ID) || (id == matchArray[i].Competitor2ID)) {
+            $('#matches').append('<p>' + matchArray[i].Competitor1ID + " " + matchArray[i].Competitor2ID + '</p>');
+        }
+    }
 }
+
+function errorOnAjax() {
+    console.log("ERROR in ajax request.");
+}
+
+window.setTimeout(ajaxMatches, 0);
+
+
+
+
+
+
+
+
