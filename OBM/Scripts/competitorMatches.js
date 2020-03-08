cbb30175837802714f61ba299c47c18fb5edf66b
Expanseman <ethan.blackmail@gmail.com>
2020-03-07 16:54:08 -0800

    displaying color cards based on status of match

--- OBM/Scripts/competitorMatches.js
+++ OBM/Scripts/competitorMatches.js
@@ -20,8 +20,31 @@ function dispMatches(jsonString) {
     var arrLength = matchArray.length;
     for (var i = 0; i < arrLength; i++) {
         //console.log(matchArray[i]);
+        var cardCol = "";
         if ((id == matchArray[i].Competitor1ID) || (id == matchArray[i].Competitor2ID)) {
-            $('#matches').append('<p>' + matchArray[i].Competitor1ID + " " + matchArray[i].Competitor2ID + '</p>');
+            if ((matchArray[i].Score1 == null) || (matchArray[i].Score2 == null)) {
+                if ((matchArray[i].Time > new Date()) || (matchArray[i].Time == null)) {
+                    cardCol = "bg-primary";
+                }
+                else {
+                    cardCol = "bg-warning";
+                }
+            }
+            else if ((id == matchArray[i].Competitor1ID) && (matchArray[i].Score1 > matchArray[i].Score2)) {
+                cardCol = "bg-success";
+            }
+            else if ((id == matchArray[i].Competitor2ID) && (matchArray[i].Score2 > matchArray[i].Score1)) {
+                cardCol = "bg-success";
+            }
+            else {
+                cardCol = "bg-danger";
+            }
+
+            //console.log(new Date('2020-03-07 16:20:15.000').getDate());
+            //console.log(new Date().getDate());
+
+            $('#matches').append('<div class="card text-white ' + cardCol + '  mb-3"><div class="card-body"><h5>' 
+                + matchArray[i].Competitor1ID + " " + matchArray[i].Competitor2ID + '</h5></div></div>');
         }
     }
 }
