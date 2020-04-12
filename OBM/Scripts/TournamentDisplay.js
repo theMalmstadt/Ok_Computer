da55d3ae4556be69c98c015508265a86eca89d7c
Expanseman <ethan.blackmail@gmail.com>
2020-04-12 11:34:34 -0700

    fixed graph not showing for 2 round tournaments

--- OBM/Scripts/TournamentDisplay.js
+++ OBM/Scripts/TournamentDisplay.js
@@ -35,12 +35,7 @@ function lineage(data, current, currY, childY, e, compList) {
     }
 
     if (current.Winner == null) {
-        if (current.Time == null) {
-            nodeLineColor = 'gray';
-        }
-        else {
-            nodeLineColor = '#3895d3';
-        }
+        nodeLineColor = 'gray';
     }
     else {
         var winner = compList.find(x => x.compID === current.Winner);
@@ -180,8 +175,8 @@ function drawTree(data) {
     dataList = dataList.concat(hidden);
 
     largestRound = largestRound - 2;
-    if (largestRound < 0) {
-        largestRound = 0;
+    if (largestRound < 1) {
+        largestRound = 1;
     }
 
     var ctx = document.getElementById('myChart');
