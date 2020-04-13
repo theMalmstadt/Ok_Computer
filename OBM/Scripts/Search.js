79318969a80803a2be74409cd4cdd4f54d744de0
chemotroph <zakkeipp@gmail.com>
2020-04-12 18:01:20 -0700

    Finished the new search page, it lets you search from events or tournaments, and could be easilly modified to search more tables.

--- OBM/Scripts/Search.js
+++ OBM/Scripts/Search.js
@@ -1,8 +1,46 @@
-﻿console.log("hihihihihi");
+﻿var search = $("#searchterm").val();
+$("#searchterm").html("");
+var myTable;
+$(document).ready(function () {
+    console.log("hihihihihi");
 
-var search = $("#searchterm").val();
-var myTable; 
-drawEvents()
+   
+    if($("#table").val()=="Events")
+        drawEvents();
+    $("#table").change(redraw);
+
+
+});
+
+function redraw()
+{
+    $("#searchResults").empty();
+    myTable.fnDestroy();
+    if ($("#table").val() == "Events") drawEvents();
+
+    else  drawTournaments();
+}
+
+function drawTournaments() {
+    var result = $.ajax({
+        type: 'GET',
+        dataType: "json",
+        contentType: "application/json",
+        url: '/Tournaments/PublicTournaments',
+        error: errorOnAjax,
+        success: function (response) {
+            drawTableTournaments(response);
+        }
+    });
+
+}
+
+
+
+
+
+
+
 function drawEvents()
 {   
     var result = $.ajax({
@@ -12,24 +50,26 @@ function drawEvents()
         url: '/Events/PublicEvents',
         error: errorOnAjax,
         success: function (response) {
-            drawTable(response);
+            drawTableEvents(response);
         }
     });
     
 }
-function drawTable(data) {
+function drawTableEvents(data) {
     console.log(data);
-    
+   
 myTable=$('#searchResults').dataTable({
    "data": data,
    "columns": [
        { "data": "EventName" },
        { "data": "Location" },
-      { 
+       { "data": "UserName" },
+
+       { 
          "data": "url",
          "render": function(data, type, row, meta){
             if(type === 'display'){
-                data = '<a href="' + data + '">' + data + '</a>';
+                data = '<a href="' + data + '">' + "Details" + '</a>';
             }
             
             return data;
@@ -41,6 +81,40 @@ myTable=$('#searchResults').dataTable({
 
     
 }
+
+
+
+
+
+function drawTableTournaments(data) {
+    console.log(data);
+
+    myTable = $('#searchResults').dataTable({
+        "data": data,
+        "columns": [
+            { "data": "TournamentName" },
+
+            { "data": "Game" },
+{
+                "data": "url",
+                "render": function (data, type, row, meta) {
+                    if (type === 'display') {
+                        data = '<a href="' + data + '">' + "Details" + '</a>';
+                    }
+
+                    return data;
+                }
+            }
+        ]
+    });
+    myTable.fnFilter(search);
+
+
+}
+
+
+
+
 
 
 function errorOnAjax()
