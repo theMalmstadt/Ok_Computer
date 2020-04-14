5003943590ed806601397f507d5674c4cb10cfac
chemotroph <zakkeipp@gmail.com>
2020-04-13 18:02:05 -0700

    omplete advanced tournament creation

--- OBM/Scripts/CreateTournament.js
+++ OBM/Scripts/CreateTournament.js
@@ -32,16 +32,31 @@ function gotoChallonge(url)
 
 function drawForm() {
 
-    $("#scriptContainer").append('<div class="col-sm">');
-    $("#scriptContainer").append('<row><input type="String" class="form-control" id="TournamentName" placeholder="Tournament Name" required="true"></row>');
-    $("#scriptContainer").append('<row><input type="String" class="form-control" id="ApiKey" value='+myApiKey+'></row>');
-    $("#scriptContainer").append('<row><input type="String" class="form-control" id="Description" placeholder="Description" required="true"></row>');
-    $("#scriptContainer").append('<row><input type="String" class="form-control" id="Game" placeholder="Game" required="true"></row>');
-    $("#scriptContainer").append('<row><input type="String" class="form-control" id="url" placeholder="URL" required="true"></row>');
+    $("#scriptContainer").append('<div class="col">');
+    $("#scriptContainer").append('<div class="row"><input type="String" class="form-control" id="TournamentName" placeholder="Tournament Name" required="true"></div>');
+    $("#scriptContainer").append('<div class="row"><input type="String" class="form-control" id="Description" placeholder="Description" required="true"></div>');
+    $("#scriptContainer").append('<div class="row"><input type="String" class="form-control" id="Game" placeholder="Game" required="true"></div>');
+    $("#scriptContainer").append('<div class="row"><input type="String" class="form-control" id="url" placeholder="URL" required="true"></div>');
+
+    $("#scriptContainer").append("<div class='row'><select id='RankedBy' class='form-control'> <option value='match wins'>Match wins</option>  <option value='game wins'>Game wins</option>  <option value='points scored'>Points scored</option>    <option value= 'points difference'>Points difference</option>   <option value= 'custom'>custom</option></select></div>");
+    $("#scriptContainer").append('<div class="row">Points for Bye:<input type="Integer" class="form-control" id="pointsForBye" placeholder="Points for Bye" required="true"></div>');
+    $("#scriptContainer").append('<div class="row">Signup Cap<input type="Integer" class="form-control" id="signupCap" placeholder="Sign-up cap" required="true"></div>');
+
+    $("#scriptContainer").append("<div class='row'>Start date and time:<input class='form-control' id='dateTime' type='text' ></div>");
+    var myDatepicker = $('#dateTime').datetimepicker
+        ({
+            ampm: true, // FOR AM/PM FORMAT
+            use12Hours :  true,
+            format: 'd/M/Y H:m:s'        });
+  
+    $("#scriptContainer").append('<div class="row">Check-in Duration<input type="Integer" class="form-control" id="checkinDuration" placeholder="Minutes" required="true"></div>');
+
+    $("#scriptContainer").append('<div  class="row"><input type="checkbox" class="form-check-input" id="Private" label="Private?" required="true"><label class="form-check-label" for="Private">Is your tournament private?</label>  </div>');
+    $("#scriptContainer").append('<div  class="row"><input type="checkbox" class="form-check-input" id="OpenSignUp" label="OpenSignUp?" required="true"><label class="form-check-label" for="OpenSignUp">Open signup?</label></div>');
 
 
-    $("#scriptContainer").append('<row><input type="checkbox" class="form-check-input" id="Private" label="Private?" required="true"><label class="form-check-label" for="Private">Is your tournament private?</label></row>');
-    $("#scriptContainer").append('<row  class="row" ><button id="submit" type="button" class="btn btn-secondary">Submit to Challonge</button></row>');
+
+    $("#scriptContainer").append('<div  class="row" ><button id="submit" type="button" class="btn btn-secondary">Submit to Challonge</button></div>');
     $("#submit").click(request);
     $("#scriptContainer").append('<div id=linkBox></div>');
     $("#scriptContainer").append('</div>');
@@ -59,15 +74,27 @@ function errorOnAjax()
 
 var request = function () {
     var tournament = {};
-    tournament.api_key = $('#ApiKey').val();
     tournament.myURL = $('#url').val();
     tournament.name = $('#TournamentName').val();
+    tournament.start_at = $("#dateTime").val();
+
+    tournament.ranked_by = $("#RankedBy").val();
+    tournament.open_signup = $("#OpenSignUp").is(":checked");
+    tournament.pts_for_bye = $("#pointsForBye").val()
+    tournament.signup_cap = $("#signupCap").val();
+    tournament.check_in_duration = $("#checkinDuration").val();
+
+
+
 
     tournament.description = $('#Description').val();
     tournament.subdomain = $('#Sub-Domain').val();
     tournament.game_name = $('#Game').val();
     tournament.private = $("#Private").is(":checked");
     tournament.event_id = $("#eventID").val();
+
+
+
     data = JSON.stringify(tournament, null, '\t');
     console.log(tournament);
     console.log(JSON.stringify(tournament));
