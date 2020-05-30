d0a37fa459ac340b0f691949f01752838f924d3a
theMalmstadt <jacob_malmstadt@hotmail.com>
2020-05-29 21:20:28 -0700

    You can not search for Organizers. Organizers also have a profile page that Displays which events they are hosting.

---
+++ OBM/Controllers/OrganizerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OBM.DAL;
+using OBM.Models;
+using OBM.Models.ViewModels;
+
+namespace OBM.Controllers
+{
+    public class OrganizerController : Controller
+    {
+        EventContext db = new EventContext();
+        // GET: Organizer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OrganizersSearch()
+        {
+            List<Organizer> organizers = new List<Organizer>();
+
+            foreach(var user in db.AspNetUsers)
+            {
+                Organizer organizer = new Organizer();
+
+                organizer.organizerName = user.UserName;
+                organizer.organizerID = user.Id;
+
+                organizers.Add(organizer);
+            }
+
+            return View(organizers);
+        }
+
+        [HttpGet]
+        public ActionResult PublicProfile(string OrgID)
+        {
+            Organizer organizer = new Organizer();
+
+            organizer.organizerID = OrgID;
+            organizer.organizerName = db.AspNetUsers.Find(OrgID).UserName;
+
+            OrganizerViewModel organizerViewModel = new OrganizerViewModel(organizer);
+
+            return View(organizerViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult PrivateProfile(string OrgID)
+        {
+            Organizer organizer = new Organizer();
+
+            organizer.organizerID = OrgID;
+            organizer.organizerName = db.AspNetUsers.Find(OrgID).UserName;
+
+            OrganizerViewModel organizerViewModel = new OrganizerViewModel(organizer);
+
+            return View(organizerViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void ChangePrivateProfile()
+        {
+
+        }
+    }
+}
---
+++ OBM/Models/Organizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OBM.Models
+{
+    public partial class Organizer
+    {
+        [StringLength(128)]
+        public string organizerName { get; set; }
+
+        public string organizerID { get; set; }
+    }
+}
